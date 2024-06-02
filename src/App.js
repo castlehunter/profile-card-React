5b665b876b4b93356db1be0fc549462644689017
castlehunter <vvxiaobai@gmail.com>
2024-06-01 22:32:46 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-box">
+      <img src="/logo512.png" alt="s"></img>
+      <h2>Title</h2>
+      <p>
+        In the heart of the bustling city, the old library stood as a beacon of
+        knowledge and tranquility. Its ancient stone walls were covered in ivy,
+        and large, arched windows let in streams of golden sunlight that danced
+        on the dust particles in the air.
+      </p>
+      <Tag color="red" tagName="HTML+CSS" />
+      <Tag color="gray" tagName="JavaScript" />
+      <Tag color="green" tagName="Git and Github" />
+      <Tag color="purple" tagName="React" />
+      <Tag color="orange" tagName="Svelte" />
     </div>
+  );
+}
+
+function Tag(props) {
+  return (
+    <span style={{ backgroundColor: props.color }} className="tagBox">
+      {props.tagName}
+    </span>
   );
 }
 
